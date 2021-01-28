9da6c177bed89bbd66bc016fbc50eff4943019ce
rw <richewhite@comcast.net>
2021-01-28 10:02:57 -0500

    updates

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from mininet.net import Mininet
 from mininet.topo import LinearTopo
-from mininet.node import OVSController
+import time
 
 net_topo = LinearTopo(k=10)
 
-net = Mininet(topo=net_topo, controller=OVSController)  # create a 10 host net
+net = Mininet(topo=net_topo)  # create a 10 host net
 net.start()
 
-net.ping()
-net.stop()
+net.hosts[0].cmd('sudo python3 /home/rw/projects/cs6381-assignment1/pub.py &')
+net.hosts[1].cmd('sudo python3 /home/rw/projects/cs6381-assignment1/sub.py 10.0.0.1 &')
+
+while(True):
+    time.sleep(1)
