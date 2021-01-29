a2c2d9ce3e704cb1afb8c3d1dd425e66c36fa892
rw <richewhite@comcast.net>
2021-01-29 16:17:00 -0500

    start to abstract ports/devices

--- broker.py
+++ broker.py
@@ -1,14 +1,19 @@
 import zmq
+import sys
 
 context = zmq.Context()
 
+in_bound = sys.argv[1] if len(sys.argv) > 1 else "5555"
+out_bound = sys.argv[2] if len(sys.argv) > 2 else "5556"
+
+
 # connects our Publishers to the Broker
 front_end = context.socket(zmq.XSUB)
-front_end.bind("tcp://*:5555")
+front_end.bind(f"tcp://*:{in_bound}")
 
 # connects our  Subscribers to the Broker
 back_end = context.socket(zmq.XPUB)
 back_end.setsockopt(zmq.XPUB_VERBOSE, 1)
-back_end.bind("tcp://*:5556")
+back_end.bind(f"tcp://*:{out_bound}")
 
 zmq.proxy(front_end, back_end)
--- pub.py
+++ pub.py
@@ -10,9 +10,9 @@ port = "5555"
 proxy = sys.argv[1] if len(sys.argv) > 1 else "*"
 
 conn_str = f'tcp://{proxy}:{port}'
-print(f"Publisher pushing to {conn_str}")
+print(f'Publisher pushing to {conn_str}')
 
-socket.bind(conn_str)
+socket.connect(conn_str)
 
 
 while True:
